cc2b88e4fa9a5a331fdd70c305b488c492e34e8f
Soham Patel <39306562+soham1255@users.noreply.github.com>
2023-01-30 23:07:30 +0530

    Update image-slider.php

--- image-slider.php
+++ image-slider.php
@@ -1,3 +1,5 @@
+<?php
+
 /**
  * Plugin Name:       Image Slider
  * Plugin URI:        https://github.com/soham1255/Slideshow
@@ -10,3 +12,37 @@
  * Text Domain:       image-slider
  * Domain Path:       /languages
  */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'IMAGE_SLIDER_VERSION', '1.0.0' );
+
+
+function activate_image_slider() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-image-slider-activator.php';
+	Image_Slider_Activator::activate();
+}
+
+
+function deactivate_image_slider() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-image-slider-deactivator.php';
+	Image_Slider_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_image_slider' );
+register_deactivation_hook( __FILE__, 'deactivate_image_slider' );
+
+
+require plugin_dir_path( __FILE__ ) . 'includes/class-image-slider.php';
+
+
+function run_image_slider() {
+
+	$plugin = new Image_slider();
+	$plugin->run();
+
+}
+run_image_slider();
