e39a01865ec2eec2cc0be6131d306d7b4bb68fa9
Soham Patel <39306562+soham1255@users.noreply.github.com>
2023-02-01 21:33:36 +0530

    Update image-slider-public.js

--- public/js/image-slider-public.js
+++ public/js/image-slider-public.js
@@ -1,7 +1,17 @@
-jQuery(document).ready(function ($) {
-    var swiper = new Swiper('.swiper-container', {
-        pagination: {
-        el: '.swiper-pagination',
+(function($){
+    var slider_front_js = {
+        __init: function(){
+            $(document).ready(this.initialize_ele);
         },
-    });
-});
+        initialize_ele: function(){
+            jQuery(document).ready(function ($) {
+                var swiper = new Swiper('.swiper-container', {
+                    pagination: {
+                    el: '.swiper-pagination',
+                    },
+                });
+            });
+        }
+    }
+    slider_front_js.__init();
+})(jQuery); 
